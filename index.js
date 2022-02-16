50edb8615494cde4f3bf2a395110f53072d5b7e5
gayeon <gayean01@naver.com>
2022-02-16 13:27:02 +0900

    암호화 이후

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 const express = require('express')
 const app = express()
 const port = 5000
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://gayeon:000101@cluster0.jmesi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const{User}=require("./models/User");
+const bodyParser=require('body-parser');
+const config=require('./config/key');
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+//application//json
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose')
+const res = require('express/lib/response');
+mongoose.connect(config.mongoURI)
+.then(()=>console.log('MongDB connected...'))
+.catch(err=>console.log(err))
+
+
+  app.get('/', (req, res) => {
+  res.send('Hello World!안녕')
 })
+
+app.post('/register', (req, res) => {
+
+    //회원 가입 할떄 필요한 정보들을  client에서 가져오면 
+    //그것들을  데이터 베이스에 넣어준다. 
+    const user = new User(req.body)
+  
+
+
+
+
+    user.save((err, userInfo) => {
+      if (err) return res.json({ success: false, err })
+      return res.status(200).json({
+        success: true
+      })
+    })
+  })
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
