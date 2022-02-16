cc787d7b0f850fb5b1b3eca92ea49d671ca0771e
gayeon <gayean01@naver.com>
2022-02-16 10:22:53 +0900

    다시 처음으로 넣기

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://gayeon:000101@cluster0.jmesi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
